50aec0067eb162aa12f410fe979a89e4b14e3285
Hugo Perez <Hugo Perez>
2016-08-26 14:02:21 -0500

    primer commit by Hugo

---
+++ core/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core
+{
+    public class Class1
+    {
+        static int suma(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
